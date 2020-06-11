37ff5dd36b8eed3018cabd9ccbc8072973202708
Daimon31999 <dimahinev31999@gmail.com>
2020-06-11 16:56:23 +0300

    Updates

---
+++ precache-manifest.6a15a20909d023eae0c97fbad1456096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffafa2cdf573ad3fa1c083a8767e9e0",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "a38063b4ec7475b322f3",
+    "url": "/notes/static/css/main.595faa4b.chunk.css"
+  },
+  {
+    "revision": "1c7845535c413f2f980f",
+    "url": "/notes/static/js/2.5816d526.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notes/static/js/2.5816d526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38063b4ec7475b322f3",
+    "url": "/notes/static/js/main.fe95a763.chunk.js"
+  },
+  {
+    "revision": "49840ceffa9d5e5c452d",
+    "url": "/notes/static/js/runtime-main.2fe05bd3.js"
+  }
+]);
